abdc1047154686e8bcd8820fca4f04e662245d89
AlCh440 <albert.chica@estudiantat.upc.edu>
2022-02-18 20:13:08 +0100

    Deleting images and moving some documents

--- GameDev_Platformer/Output/Assets/maps/Forest1.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="Forest 1" tilewidth="16" tileheight="16" tilecount="140" columns="20">
- <image source="../Spritesx16/Forest_Tileset_x2.png" width="320" height="112"/>
-</tileset>
--- GameDev_Platformer/Output/Assets/maps/Forest_Assets_x2.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="Forest_Assets_x2" tilewidth="16" tileheight="16" tilecount="189" columns="27">
- <image source="../Spritesx16/Forest_Assets_x2.png" width="432" height="112"/>
-</tileset>
