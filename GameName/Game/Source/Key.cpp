ad90b6d738925fe1cef2ec0882cc9be437832a45
AlCh440 <albert.chica@estudiantat.upc.edu>
2022-05-11 23:45:54 +0200

    Added key

--- GameName/Game/Source/Key.cpp
+++ GameName/Game/Source/Key.cpp
@@ -1,1 +1,38 @@
-#pragma once
+#include "Key.h"
+
+Key::Key(iPoint pos) : Item(pos)
+{
+}
+
+Key::Key(Collider_Type type, iPoint pos) : Item(type, pos)
+{
+}
+
+bool Key::Start()
+{
+	return true;
+}
+
+bool Key::PreUpdate()
+{
+	return true;
+}
+
+bool Key::Update(float dt)
+{
+	return true;
+}
+
+bool Key::CleanUp()
+{
+	return true;
+}
+
+bool Key::PostUpdate()
+{
+	return true;
+}
+
+void Key::OnCollision(PhysBody* bodyA, PhysBody* bodyB)
+{
+}
--- GameName/Game/Source/Key.h
+++ GameName/Game/Source/Key.h
@@ -1,1 +1,31 @@
 #pragma once
+#pragma once
+#include "Entity.h"
+#include "Item.h"
+
+class Key : public Item
+{
+public:
+
+	Key(iPoint pos);
+	Key(Collider_Type type, iPoint pos);
+
+	virtual bool Start();
+	virtual bool PreUpdate();
+	// Updates animation and collider position
+	virtual bool Update(float dt);
+	virtual bool CleanUp();
+	// Called from ModuleEntitys' Update
+	virtual bool PostUpdate();
+
+	// Collision response
+	virtual void OnCollision(PhysBody* bodyA, PhysBody* bodyB);
+
+	int id;
+
+protected:
+	int spriteRotation = 0;
+	SDL_RendererFlip spriteDir;
+
+
+};
